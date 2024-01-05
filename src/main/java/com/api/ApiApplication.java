705d6fbc4116b44717f0d47460eb3280b9a25df6
Anis <contact@aniskh.com>
2024-01-05 14:32:39 +0100

    add auditing

--- src/main/java/com/api/ApiApplication.java
+++ src/main/java/com/api/ApiApplication.java
@@ -1,12 +1,36 @@
 package com.api;
 
+import com.api.user.Role;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.api.auth.AuthenticationService;
+import com.api.auth.RegisterRequest;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(Role.A)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+		};
 	}
 }
