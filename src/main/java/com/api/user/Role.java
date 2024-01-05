73f480e32e5971071ba5bb6a9ed05286e9193a1a
Anis <contact@aniskh.com>
2024-01-05 14:22:31 +0100

    add user entity

---
+++ src/main/java/com/api/user/Role.java
@@ -0,0 +1,10 @@
+package com.api.user;
+
+public enum Role {
+  A, // Admin globale
+  E, // Etudiant
+  RG, // Responsable de Gestion
+  RF, // Responsable de Formation
+  F, // Formateur
+  RM // Responsable de Maintenance
+}
